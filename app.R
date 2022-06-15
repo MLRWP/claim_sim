b7f48768937e456e8d7b877cba5033147b0a96eb
ResearchActuary <bfannin@casact.org>
2022-06-15 19:10:16 -0400

    Change order of statements in app.R

--- app.R
+++ app.R
@@ -14,21 +14,22 @@ options(shiny.reactlog = TRUE)
 ui <- navbarPage(
     title = "Claim sim"
     , tab_main
-    # , tab_gabrielli_wutrich
-    , tab_imaginator
     , tab_synthetic
+    , tab_imaginator
+    # , tab_gabrielli_wutrich
     , tab_cascsim
     # , tab_data_tables
     # , tab_compare
 )
 
 server <- function(input, output, session) {
-    # eval(expr_gabrielli_wutrich)
-    eval(expr_imaginator)
-    eval(expr_cascsim)
-    eval(expr_synthetic)
-    # eval(expr_compare)
-    # eval(expr_data_tables)
+
+  eval(expr_synthetic)
+  eval(expr_imaginator)
+  # eval(expr_gabrielli_wutrich)
+  eval(expr_cascsim)
+  # eval(expr_compare)
+  # eval(expr_data_tables)
 }
 
 shinyApp(ui, server)
