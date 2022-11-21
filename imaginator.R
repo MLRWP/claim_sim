55e410c0f692b13a5d35f560fb9a311a86088b5b
ResearchActuary <bfannin@casact.org>
2022-11-21 12:21:11 -0500

    Add data table output and download buttons.

--- imaginator.R
+++ imaginator.R
@@ -5,62 +5,79 @@ tab_imaginator <- tabPanel(
   'imaginator',
   fluidRow(
     column(
-      width = 12,
-      numericInput(
-        "num_starting_policies",
-        "Set the number of initial policies",
-        min = 10,
-        max = 10e3,
-        step = 5,
-        value = 10
-      ),
-    )
-  ),
-  fluidRow(
-      # h3("Policy growth and retention"),
-      # fluidRow(
-      #   column(6, 
-      #     wellPanel(
-      #     ),
-      #   ), 
-      #   column(6, 
-      #     wellPanel(
-      #     ),
-      #   ),
-      # ),
-    column(
       width = 4,
-      h3("Claim frequency"),
-      wellPanel(
-        distributions3_ui("claim_frequency")
-      ),
-      h3("Payment frequency"),
-      wellPanel(
-        distributions3_ui("payment_frequency")
-      ),
-    ), 
-    column(
-      width = 4,
-      h3("Occurrence wait time"),
-      wellPanel(
-        distributions3_ui("wait_occurrence")
-      ),
-      h3("Report wait time"),
-      wellPanel(
-        distributions3_ui("wait_report")
-      ),
-    ),
-    column(
-      width = 4,
-      h3("Payment wait"),
-      wellPanel(
-        distributions3_ui("wait_payment")
-      ),
-      h3("Payment severity"),
-      wellPanel(
-        distributions3_ui("payment_severity")
+      fluidRow(
+        column(
+          width = 12,
+          numericInput(
+            "num_starting_policies",
+            "Set the number of initial policies",
+            min = 10,
+            max = 10e3,
+            step = 5,
+            value = 10
+          ),
+        ),
+        # h3("Policy growth and retention"),
+        # fluidRow(
+        #   column(6, 
+        #     wellPanel(
+        #     ),
+        #   ), 
+        #   column(6, 
+        #     wellPanel(
+        #     ),
+        #   ),
+        # ),
+        column(
+          width = 4,
+          h4("Claim frequency"),
+          wellPanel(
+            distributions3_ui("claim_frequency")
+          ),
+          h4("Payment frequency"),
+          wellPanel(
+            distributions3_ui("payment_frequency")
+          ),
+        ), 
+        column(
+          width = 4,
+          h4("Occurrence wait time"),
+          wellPanel(
+            distributions3_ui("wait_occurrence")
+          ),
+          h4("Report wait time"),
+          wellPanel(
+            distributions3_ui("wait_report")
+          ),
+        ),
+        column(
+          width = 4,
+          h4("Payment wait"),
+          wellPanel(
+            distributions3_ui("wait_payment")
+          ),
+          h4("Payment severity"),
+          wellPanel(
+            distributions3_ui("payment_severity")
+          )
+        ),
       )
     ),
+    column(
+      width = 8,
+      h1("Imaginator data"), 
+      downloadButton('download_imaginator_policies', 'Download policies'),
+      downloadButton('download_imaginator_claims', 'Download claims'),
+      downloadButton('download_imaginator_claim_transactions', 'Download claim transactions'),
+      radioButtons(
+        'rdo_data_table_im',
+        'Which data table would you like to see?',
+        choices = c('Policies' = 'policies', 'Claims' = 'claims', 'Claim transactions' = 'transactions'),
+        selected = 'transactions'
+      ),
+      dataTableOutput("tbl_data_table_im")
+    )
   )
 )
 
@@ -112,6 +129,59 @@ expr_imaginator <- quote({
           payment_amount = sum(payment_amount, na.rm = TRUE)
         )
     })
+    
+    output$download_imaginator_policies <- downloadHandler(
+      filename = function() {
+        paste0('imaginator-policies-', Sys.Date(), '.csv')
+      },
+      content = function(con) {
+        write_csv(tbl_policies(), con)
+      },
+      contentType = 'text/csv'
+    )
+    
+    output$download_imaginator_claims <- downloadHandler(
+      filename = function() {
+        paste0('imaginator-claims-', Sys.Date(), '.csv')
+      },
+      content = function(con) {
+        write_csv(tbl_claims(), con)
+      },
+      contentType = 'text/csv'
+    )
+    
+    output$download_imaginator_claim_transactions <- downloadHandler(
+      filename = function() {
+        paste0('imaginator-claim-transactions-', Sys.Date(), '.csv')
+      },
+      content = function(con) {
+        write_csv(tbl_claim_transactions(), con)
+      },
+      contentType = 'text/csv'
+    )
+    
+    if (input$rdo_data_table_im == 'policies') {
+      output$tbl_data_table_im <- renderDataTable(
+        tbl_policies(),
+        options = list(
+          filter = 'top'
+        )
+      )
+    } else if (input$rdo_data_table_im == 'claims') {
+      output$tbl_data_table_im <- renderDataTable(
+        tbl_claims(),
+        options = list(
+          filter = 'top'
+        )
+      )
+    } else {
+      output$tbl_data_table_im <- renderDataTable(
+        tbl_claim_transactions(),
+        options = list(
+          filter = 'top'
+        )
+      )
+    }
     
   })
 
